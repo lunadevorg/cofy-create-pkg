9b9fe90d9f8d0c12196349773488cb6be131c845
ognevnydemon <maksapple2306@gmail.com>
2024-04-05 16:40:57 +0300

    parse Cofyfile without creating a temp var

--- src/pkg_data.rs
+++ src/pkg_data.rs
@@ -1,7 +1,6 @@
 use std::{fs::read_to_string, path::Path};
 
 use serde::Deserialize;
-use toml::de::Error;
 
 #[derive(Deserialize)]
 pub struct PackageData {
@@ -14,8 +13,6 @@ impl PackageData {
     pub fn new(file: &Path) -> Result<PackageData, Box<dyn std::error::Error>> {
         let file_str = read_to_string(file)?;
 
-        let res: Result<PackageData, Error> = toml::from_str(&file_str);
-
-        Ok(res?)
+        Ok(toml::from_str(&file_str)?)
     }
 }
